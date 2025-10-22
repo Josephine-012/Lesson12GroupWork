0f42a22005ad4e7bc3508263981b63ae9bc297c8
Josephine-012 <josephineofori122@gmail.com>
2025-10-22 17:36:26 -0500

    Updated problem3.py

--- problem3.py
+++ problem3.py
@@ -1,1 +1,8 @@
-
+def is_even(number) :
+    return number % 2 == 0
+if __name__ == "__main__":
+    num = int(input("Enter a number: "))
+    if is_even(num):
+        print("True")
+    else:
+        print("False")
