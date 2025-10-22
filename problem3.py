6e043fac5693b6aafbca2b8fe5f2cadbd4df3dd8
Josephine-012 <josephineofori122@gmail.com>
2025-10-22 18:02:35 -0500

    Completed problem3.py with the full UCASE strategy

--- problem3.py
+++
@@ -1,8 +0,0 @@
-def is_even(number) :
-    return number % 2 == 0
-if __name__ == "__main__":
-    num = int(input("Enter a number: "))
-    if is_even(num):
-        print("True")
-    else:
-        print("False")
